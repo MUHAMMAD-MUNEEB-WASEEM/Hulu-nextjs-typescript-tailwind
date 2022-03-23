12d4a6fee7edbc0a76498438d4ca322eb77e3e66
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2022-03-23 14:21:29 +0500

    Done with the navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +17,8 @@ const Home: NextPage = () => {
       <Header/>
 
       {/*Navbar*/}
+
+      <Nav/>
 
       {/*Results*/}
 
